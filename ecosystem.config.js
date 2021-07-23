180303a5e33cbee6cc950c47e74f172e0ab2abfc
mirazizergashev <mirazizmirmuxsinovich@gmail.com>
2021-07-23 14:23:14 +0500

    salom

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [
+    {
+      name: 'Ecommerce_shop',
+      script: './index.js',
+      watch: true,
+      env: {
+        PORT: 8090,
+        NODE_ENV: 'development'
+      },
+      env_production: {
+        PORT: 8091,
+        NODE_ENV: 'production'
+      }
+    }
+  ]
+};
