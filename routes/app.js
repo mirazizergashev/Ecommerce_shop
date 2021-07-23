550a798416f4615f47815cebcedd6b1fd9051756
mirazizergashev <mirazizmirmuxsinovich@gmail.com>
2021-07-23 08:15:53 +0500

    Eccomerce_shop project started! Good luck to everyone

--- routes/app.js
+++ routes/app.js
@@ -3,22 +3,9 @@ const app = express();
 const auth=require('../middleware/auth');
 
 const sign = require("./sign/app");
-const roles=require('./roles/app');
-const orders=require('./orders/app');
-const category=require('./categories/app');
-const product=require('./product/app');
-const table=require('./table/app');
 
 // sing in , up 
 app.use("/sign", sign);
-app.use("/roles",auth.authCheck, roles); 
-app.use("/orders",auth.authCheck, orders); 
-app.use("/category",auth.authCheck, category); 
-app.use("/table",auth.authCheck, table); 
-app.use("/product",auth.authCheck, product); 
-
-//danniy olish
-// app.use("/data", doc);
 
 
 
