883d0206f6bfb9f379fbe669381b401f32e350e4
mirazizergashev <mirazizmirmuxsinovich@gmail.com>
2021-07-26 11:02:53 +0500

    sdfgh

--- controllers/homeController.js
+++
@@ -1,8 +0,0 @@
-var homeController=function(){}
-
-homeController.index=function(req,res){
-    req.flash('success', 'Welcome to HOm')
-    res.render('home/index',{title:'Home page'});
-}
-
-module.exports=homeController
