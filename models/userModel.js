9f36cb78d6c80949dbdff1df0bc395c09dc02bf6
mirazizergashev <mirazizmirmuxsinovich@gmail.com>
2021-07-30 10:38:21 +0500

    getAllUsers

--- models/userModel.js
+++ models/userModel.js
@@ -47,7 +47,7 @@ userModel.getMe=function(userId,result){
 
 
 userModel.getAllUsers=function(result){
-    pool.query(`SELECT concat(u.last_name,\" \",u.first_name) fio,u.phone,u.isActive status,r.name role 
+    pool.query(`SELECT concat(u.last_name,\" \",u.first_name) fio,u.phone,u.isActive status,r.name role,created_on 
     FROM users u inner join roles r on r.id=u.role_id where r.id!=1`,function(err,res){
         if(err){
             return result(err,null);
