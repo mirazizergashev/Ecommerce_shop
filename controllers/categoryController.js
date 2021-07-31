0a3ca1edee5218e522d70ae019c8499c3be7e5ed
Coder <oybekakramov0@gmail.com>
2021-07-30 20:38:57 -0500

    Category changed

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -179,7 +179,156 @@ categoryController.getSub = function (req, res) {
      })
  }
 
+//xususiyatlar
 
+ categoryController.categoryPropertiesCU = function (req, res) {
+
+    //validatsiyada xatolik
+    const checked = schema.category_properties.validate(req.body);
+    if (checked.error) {
+        const msg = checked.error.details[0].message.split("#")
+        return res.status(200).json({
+            code: 400,
+            error: {
+                message: {
+                    uz: msg[0],
+                    en: msg[1],
+                    ru: msg[2]
+                }
+            }
+
+        });
+    }
+    let a = req.body;
+    var data = [
+        a.id ,
+        a.nom ,
+        a.category_id,
+        a.tip_id,
+        a.hol
+    ]
+
+    categoryModel.category_properties_edit_insert(data, function (err, result) {
+        if (err) {
+            console.log(err)
+            return res.status(200).json({
+                code: 500,
+                error: {
+                    message: {
+                        uz: "Serverda xatolik tufayli rad etildi !",
+                        en: "Rejected due to server error!",
+                        ru: "Отклонено из-за ошибки сервера!"
+                    }
+                }
+            })
+        } else {
+            // req.flash('success', 'Employee added succesfully');
+            switch (result[0][0].natija) {
+                case '1':
+                    return res.status(200).json({
+                        code: 203,
+                        success: {
+                            message: {
+                                uz: "Ma'lumotlar saqlandi!",
+                                en: "A new user has been created!",
+                                ru: "Создан новый пользователь!"
+                            }
+                        }
+                    })
+
+                case '2':
+                    return res.status(200).json({
+                        code: 203,
+                        error: {
+                            message: {
+                                uz: "Ma'lumotlar o'zgartirildi !",
+                                en: "User information has changed!",
+                                ru: "Информация о пользователе изменилась!"
+                            }
+                        }
+                    })
+
+                    case '3':
+                        return res.status(200).json({
+                            code: 400,
+                            error: {
+                                message: {
+                                    uz: "Kategoriya topilmadi!",
+                                    en: "No such role found!",
+                                    ru: "Такой роли не найдено!"
+                                }
+                            }
+                        })
+                        case '4':
+                            return res.status(200).json({
+                                code: 400,
+                                error: {
+                                    message: {
+                                        uz: "Berilgan ID ga ega tip topilmadi!",
+                                        en: "No such role found!",
+                                        ru: "Такой роли не найдено!"
+                                    }
+                                }
+                            })
+                            case '5':
+                            return res.status(200).json({
+                                code: 400,
+                                error: {
+                                    message: {
+                                        uz: "Bunday ID ga ega xususiyat mavjud emas!",
+                                        en: "No such role found!",
+                                        ru: "Такой роли не найдено!"
+                                    }
+                                }
+                            })
+                           
+                default:
+
+                    return res.status(200).json({
+                        code: 418,
+                        success: {
+                            message: {
+                                uz: "Kutilmagan xatolik adminga xabar bering !",
+                                en: "Report an unexpected error to the admin!",
+                                ru: "Сообщите администратору о непредвиденной ошибке!"
+                            }
+                        }
+                    })
+
+
+
+
+            }
+
+        }
+
+    });
+
+}
+
+
+categoryController.getProperties = function (req, res) {
+    categoryModel.getProperties(req.params.id,(err,rows)=>{
+     if (err) {
+             console.log(err);
+             return res.status(200).json({
+                 code: 500,
+                 error: {
+                     message: {
+                         uz: "Serverda xatolik tufayli rad etildi !",
+                         en: "Rejected due to server error!",
+                         ru: "Отклонено из-за ошибки сервера!"
+                     }
+                 }
+             })
+         }
+        
+         res.status(200).json({
+             code: 200,
+             success: rows
+         })
+     })
+ }
 
 
 module.exports = categoryController;
--- models/categoryModel.js
+++ models/categoryModel.js
@@ -1,4 +1,5 @@
 var pool= require('../database/db');
+const { category } = require('../utils/category');
 
 var categoryModel=function(){}
 
@@ -26,6 +27,29 @@ categoryModel.getAll=function(result){
 
 categoryModel.getSub=function(id,result){
     pool.query("SELECT * FROM category where sub=?",id||0,function(err,res){
+        if(err){
+            return result(err,null);
+        }else{
+            return result(null,res);
+        }
+    });
+}
+
+
+categoryModel.category_properties_edit_insert=function(data,result){
+    pool.query("call category_properties_edit_insert(?,?,?,?,?)",data,function(err,res,field){
+        if(err){
+            return result(err,null);
+        }else{
+            return result(null,res);
+        }
+    });
+   
+}
+
+categoryModel.getProperties=function(id,result){
+    pool.query(`SELECT cp.*,t.name type FROM category_properties cp inner join types t on t.id=cp.type_id
+    and t.isActive=1  where cp.category_id=?;`,id||0,function(err,res){
         if(err){
             return result(err,null);
         }else{
