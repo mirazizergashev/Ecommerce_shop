108e435d734ed9c7bd036d6b0855b3bc10f25920
mirazizergashev <mirazizmirmuxsinovich@gmail.com>
2021-08-05 09:39:10 +0500

    salom maxsult

--- models/productModel.js
+++ models/productModel.js
@@ -14,6 +14,18 @@ productModel.product_edit_insert=function(data,result){
    
 }
 
+//admin tasdiqlashi
+productModel.check_product=function(data,result){
+    pool.query("call check_product(?,?,?,?)",data,function(err,res,field){
+        if(err){
+            return result(err,null);
+        }else{
+            return result(null,res);
+        }
+    });
+   
+}
+
 productModel.getAll=function(result){
     pool.query("SELECT * FROM product",function(err,res){
         if(err){
