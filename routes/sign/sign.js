5f4a796d6c42540efa0bd5a62a2e2c5a9e635286
mirazizergashev <mirazizmirmuxsinovich@gmail.com>
2021-08-16 17:44:07 +0500

    Ok

--- routes/sign/sign.js
+++ routes/sign/sign.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const signController=require("../../controllers/signController")
+const userController=require("../../controllers/userController")
 
+
+router.get("/getMe",userController.getMe)
 router.get('/out', signController.signOut);
 router.post("/up", signController.signUp);
 router.post("/in", signController.signIn);
