01f00fcbbbe50f17ed51fd80478c289a15916af9
mirazizergashev <mirazizmirmuxsinovich@gmail.com>
2021-09-15 22:32:40 +0500

    salom pul

--- routes/payment/CheckPerformTransaction.js
+++ routes/payment/CheckPerformTransaction.js
@@ -2,24 +2,29 @@ const pool=require("../../database/db")
 module.exports = CheckPerformTransaction = async(data,javob)=>
 {
   
-    if(!(data.params.account.user))  {
+    if(!(data.params.account.order))  {
         return   javob.json({error:{
-            code : -31060,
+            code : -31001,
             message : 'AccessDeniet',
             data : null
         }})
 }
 
 
-    let order = data.params.account.user || data.params.account.itleader  
+    let order = data.params.account.order  
 
     const BilingErrors = require("./BilingErrors")
    
     pool.promise().query("SELECT *FROM orders WHERE id=?",[order])
     .then((rest) => {
+        // console.log(rest[0][0])
        if(rest.length==0) return  javob.json({error: BilingErrors.OrderNotFound()})
-       if(rest[0].state !== 0 ) return javob.json({error:BilingErrors.OrderAvailable()})
-       if(parseInt(rest[0].amount)*100!== data.params.amount)return javob.json({error:BilingErrors.IncorrectAmount()}) 
+       if(rest[0][0].state !== 0 ) return javob.json({error:BilingErrors.OrderAvailable()})
+       if(parseInt(rest[0][0].amount)!== data.params.amount)
+       {
+           
+           return javob.json({error:BilingErrors.IncorrectAmount()}) 
+       }
      
        return javob.json({
             result : { 
--- routes/payment/CheckTransaction.js
+++ routes/payment/CheckTransaction.js
@@ -2,12 +2,13 @@ module.exports = CheckTransaction =async(data,javob)=>
 {
     const BilingErrors = require("./BilingErrors")
     const pool=require("../../database/db")
+    
    
   
     pool.promise().query("SELECT * FROM transactions WHERE transaction_id=? limit 1",[data.params.id])
     .then(async(rest)=>
     {
-       // console.log(rest)
+       console.log(rest[0])
        if(rest[0].length==0)
         return  javob.json({error:BilingErrors.TransactionNotFound})
        else
