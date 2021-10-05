58955dfa40488db88f2dd34c2b7c244182ef5d4b
Coder <oybekakramov0@gmail.com>
2021-10-05 14:56:08 -0500

    Successfully

--- routes/payment/naqd.js
+++ routes/payment/naqd.js
@@ -251,53 +251,5 @@ app.post("/naqd", async (req, res) => {
 })
 
 
-// click etab 2
-app.use("/click/2", async (req, res) => {
-    console.log("/click/2")
-    const h = req.body
-    console.log(h)
-    if (h.action == '0' && h.error == '0') {
-        pool.promise().query(`INSERT INTO click_order (service_id,click_paydoc_id,order_id,action,
-            sign_time,error,error_note,sign_string,click_trans_id) VALUES 
-            (?,?,?,?,?,?,?,?,?); SELECT max(id) as id FROM click_order 
-            WHERE order_id=? `, [h.service_id, h.click_paydoc_id, h.merchant_trans_id, h.action,
-        h.sign_time, h.error, h.error_note, h.sign_string, h.click_trans_id, h.merchant_trans_id])
-            .then((rest) => {
-                res.json({
-                    merchant_trans_id: h.merchant_trans_id,
-                    merchant_prepare_id: rest[0][1].id,
-                    error: 0,
-                    error_note: "Success"
-                })
-            }).catch((err) => {
-                res.json({ error: 2, error_note: "Not" });
-            })
-    } else
-        res.json({ error: 2, error_note: "Not" });
-})
-
-// click etab 3
-app.use("/click/3", async (req, res) => {
-    const h = req.body
-    console.log('click3')
-    if (h.action == '1' && h.error == '0') {
-        const md5hash = md5(h.click_trans_id + h.service_id + SECRET_KEY + h.merchant_trans_id + h.merchant_prepare_id + h.amount + h.action + h.sign_time)
-        if (md5hash == req.body.sign_string) {
-            pool.promise().query(`UPDATE click_order SET action=1 WHERE id=? ;
-             UPDATE orders SET click_state=1 ,state=2 WHERE id=?; `,
-                [req.body.merchant_prepare_id, req.body.merchant_trans_id])
-                .then((rest) => {
-                    res.json({ error: 0, error_note: "Success" });
-                }).catch((err) => {
-                    res.json({ error: 1, error_note: "Not" });
-                })
-        }
-        else
-            res.json({ error: 1, error_note: "Not" });
-    }
-    else
-        res.json({ error: 1, error_note: "Not" });
-})
-
 
 module.exports = app;
