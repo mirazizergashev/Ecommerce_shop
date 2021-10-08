37508cc0d69eb37a8cbb194aacccc185e3f9bd5f
mirazizergashev <mirazizmirmuxsinovich@gmail.com>
2021-10-08 10:02:51 +0500

    Salom

--- routes/payment/order_status.js
+++ routes/payment/order_status.js
@@ -1,47 +1,120 @@
 const express = require("express");
 const app = express();
+const schema = require('../../utils/order')
+
 
 const pool = require("../../database/db");
-const { sendClickTrans } = require("../../botconnect")
 
-app.post("/order", async (req, res) => {
+app.post("/orderStatus", async (req, res) => {
+ //validatsiyada xatolik
+ const checked = schema.order_status.validate(req.body);
+ if (checked.error) {
+     const msg = checked.error.details[0].message.split("#")
+     return res.status(200).json({
+         code: 400,
+         error: {
+             message: {
+                 uz: msg[0],
+                 en: msg[1],
+                 ru: msg[2]
+             }
+         }
 
-    let fish = req.body.fish || null;
-    let mfy = req.body.mfy || null;
-    let tel = req.body.phone || null;
-    let viloyat = req.body.viloyat || null;
-    let tuman = req.body.tuman || null;
-    console.log('llll')
-    pool.promise().query(`insert into orders (user_id,amount,state,praduct_id ,isNaqd,fish,phone,viloyat,tuman,mfy,dostavka_id,curyer) 
-        values (?,?,0,?,1,?,?,?,?,?,?,0) ;
-        SELECT max(id) as id FROM orders WHERE isNaqd=1 and amount=?`, [req.session.userId || null, req.body.amount, req.body.praduct_id, fish, tel, viloyat, tuman, mfy, req.body.dostavka_id||1,req.body.amount])
-        .then((rest) => {
-            sendClickTrans(rest[0][1][0].id,1)
-            return res.status(200).json({
-                code: 200,
-                success: {
-                    message: {
-                        uz: "Muvvafaqiyatli buyurtma qilindi!",
-                        en: "Muvvafaqiyatli buyurtma qilindi!",
-                        ru: "Muvvafaqiyatli buyurtma qilindi!"
-                    }
+     });
+ }
+ let a = req.body;
+ var data = [
+     a.order_id || 0,
+     req.session.userId || 0,
+     a.status
+ ]
+
+ pool.query("call order_status(?,?,?)",data,function(err,result,field){
+    if (err) {
+        console.log(err)
+        return res.status(200).json({
+            code: 500,
+            error: {
+                message: {
+                    uz: "Serverda xatolik tufayli rad etildi !",
+                    en: "Rejected due to server error!",
+                    ru: "Отклонено из-за ошибки сервера!"
                 }
-
-            });
-        }).catch((err) => {
-            console.error("err", err)
-            return res.status(200).json({
-                code: 400,
-                error: {
-                    message: {
-                        uz: "Xatolik!",
-                        en: "Xatolik!",
-                        ru: "Xatolik!"
-                    }
-                }
-
-            });
+            }
         })
+    } else {
+        // req.flash('success', 'Employee added succesfully');
+        switch (result[0][0].natija) {
+            case '1':
+                return res.status(200).json({
+                    code: 201,
+                    success: {
+                        message: {
+                            uz: "Buyurmaning holati saqlandi!",
+                            en: "A new user has been created!",
+                            ru: "Создан новый пользователь!"
+                        }
+                    }
+                })
+
+            case '2':
+                return res.status(200).json({
+                    code: 400,
+                    error: {
+                        message: {
+                            uz: "Bunday odam topilmadi !",
+                            en: "User information has changed!",
+                            ru: "Информация о пользователе изменилась!"
+                        }
+                    }
+                })
+
+            case '3':
+                return res.status(200).json({
+                    code: 400,
+                    error: {
+                        message: {
+                            uz: "Bunday buyurtma topilmadi!",
+                            en: "No such role found!",
+                            ru: "Такой роли не найдено!"
+                        }
+                    }
+                })
+
+                case '4':
+                return res.status(200).json({
+                    code: 400,
+                    error: {
+                        message: {
+                            uz: "Bunday status topilmadi!",
+                            en: "No such role found!",
+                            ru: "Такой роли не найдено!"
+                        }
+                    }
+                })
+           
+            default:
+
+                return res.status(200).json({
+                    code: 418,
+                    error: {
+                        message: {
+                            uz: "Kutilmagan xatolik adminga xabar bering !",
+                            en: "Report an unexpected error to the admin!",
+                            ru: "Сообщите администратору о непредвиденной ошибке!"
+                        }
+                    }
+                })
+
+
+
+
+        }
+
+    }
+
+});
+
 
 })
 
--- test.js
+++ test.js
@@ -1,7 +1,11 @@
-// const pool = require("./database/db");
+let test="Men asli Buxorolikman 6 da"
 
-// pool.promise().query(`insert into category(name,sub,user_id,percent,isActive,isFoiz) 
-// values("test2",0,1,0,0,0)`).then(rest=>{
-//     console.log(rest[0].insertId)
-// })
-console.log(JSON.parse(`{"data":[{"product_id":"10","count":1,"name":"partfel","amount":67550}]}`))
+let ajrat=test.split(" ");
+let s=0;
+for (let i = 0; i < ajrat.length; i++) {
+    if(! isNaN(ajrat[i])){
+        s++;
+    }
+}
+
+console.log("'"+test+"' gapida "+s+" ta son bor")
