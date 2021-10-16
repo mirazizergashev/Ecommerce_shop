d0b39dc3a720ec9dfb832327cb289aa4f698e88c
Coder <oybekakramov0@gmail.com>
2021-10-16 17:11:09 -0500

    Ok

--- models/productModel.js
+++ models/productModel.js
@@ -14,10 +14,10 @@ Object.keys(query).forEach((x0, i) => {
     if (isNaN(parseInt(x0))) return;
     
     a.push(x0*1,query[x0])
-   ss+=` cat_prop_id=? and \`values\`=? or`
+   ss+=` cat_prop_id=? and \`values\`=? and`
 })
 if(ss){
-    ss=s0+ss.slice(0,-2)+') '
+    ss=s0+ss.slice(0,-3)+') '
 }
 
 
