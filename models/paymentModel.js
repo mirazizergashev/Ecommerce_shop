7af9295f2adf13d9e6102274ffa8043d013896f3
mirazizergashev <mirazizmirmuxsinovich@gmail.com>
2021-10-17 20:24:42 +0500

    sdfgh

--- models/paymentModel.js
+++ models/paymentModel.js
@@ -59,7 +59,7 @@ paymentModel.getOrdersAll = function (result) {
     ) as paymentType,
     d.name as deliveryType,
     o.sana as created
-     FROM orders o inner join dostavka_type d on o.dostavka_id=d.id inner join statuses s on s.id=o.status`, function (err, data) {
+     FROM orders o inner join dostavka_type d on o.dostavka_id=d.id inner join statuses s on s.id=o.status order by o.sana desc `, function (err, data) {
         if (err) {
             return result(err, null);
         } else {
@@ -73,14 +73,20 @@ paymentModel.getOrdersAllPostavshik = function (id,result) {
     pool.query(`SELECT p.id,p.name,s.count,(s.cost-s.discount) as price,ss.name as status,ss.class,
     date_format(o.sana,'%Y-%m-%d %h:%i:%s') as sana FROM suborder s inner join orders o on s.order_id=o.id 
     inner join product p on p.id=s.product_id   inner join dostavka_type d on o.dostavka_id=d.id inner join 
-    statuses ss on ss.id=o.status where p.user_id=?`,id, function (err, data) {
+    statuses ss on ss.id=o.status where p.user_id=?`,id||0, function (err, data) {
         if (err) {
             return result(err, null);
         } else {
+          
+            if(data.length==0){
+            return result(null, []);
+
+            }
             let datas=[],cont,arr4=[],cont2=[]
             data.forEach((k,ii)=>{
-                pool.query(`SELECT cp.field_name, GROUP_CONCAT(pp.values SEPARATOR '#') as content FROM product_properties pp inner join category_properties cp on cp.id=pp.cat_prop_id where pp.product_id in 
-                (SELECT id FROM product where name in (SELECT name FROM product where id=?)) GROUP BY cp.field_name;`,k.id,(err1,row2,fff)=>{
+                pool.query(`SELECT cp.field_name, GROUP_CONCAT(pp.values SEPARATOR '#') as content FROM product_properties pp inner join category_properties cp on 
+                cp.id=pp.cat_prop_id where pp.product_id =? GROUP BY cp.field_name;`,k.id,(err1,row2,fff)=>{
+                    console.log(err1)
                     row2.forEach((e, i) => {
                         cont = e.content.split('#')
                         for (let j = 0; j < cont.length; j++) {
--- routes/payment/PerformTransaction.js
+++ routes/payment/PerformTransaction.js
@@ -8,6 +8,7 @@ module.exports = PerformTransaction =async(data,javob)=>
     pool.promise().query("SELECT  *FROM transactions WHERE transaction_id=? limit 1",[data.params.id])
     .then(async(rest)=>
     {
+      
       //  console.log(rest[0][0])
        if(rest[0].length==0)
              return  javob.json({error:BilingErrors.TransactionNotFound()})
@@ -22,7 +23,8 @@ module.exports = PerformTransaction =async(data,javob)=>
          else
           if(rest[0][0].state==1) 
           {
-            // console.log("bu" , rest[0][0].state)
+            let ord=(rest[0][0].order_id)*1;
+            console.log("bu" , ord)
 
             const datee =new Date().getTime() ;
          
@@ -31,12 +33,13 @@ module.exports = PerformTransaction =async(data,javob)=>
                UPDATE orders SET state=2  WHERE  id IN  ( SELECT order_id FROM transactions 
                 WHERE transaction_id=?);
                 SELECT id FROM orders WHERE id IN  ( SELECT order_id FROM transactions 
-                  WHERE transaction_id=?);
-                  call change_order_click_payme(?);
+                  WHERE transaction_id=?); call change_order_click_payme(${ord});
               `,[datee.toString(),data.params.id,data.params.id,data.params.id])
             .then(async(rest)=>
             {  
-              sendClickTrans(rest[0][2][0].id)
+              
+              sendClickTrans(ord)
+              // sendClickTrans(rest[0][2][0].id)
                 return javob.json({result: {
                     state : 2,
                     perform_time : datee,
