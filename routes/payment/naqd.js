b92dad9f64c21a3c4de02891969dd38a1c0f94a1
mirazizergashev <mirazizmirmuxsinovich@gmail.com>
2021-10-18 10:31:30 +0500

    Ok

--- routes/payment/naqd.js
+++ routes/payment/naqd.js
@@ -136,14 +136,11 @@ app.post("/naqd", async (req, res) => {
             let viloyat = req.body.viloyat || null;
             let tuman = req.body.tuman || null;
 
-
             pool.promise().query(`insert into orders (user_id,state,sana,fish,phone,viloyat,tuman,mfy,dostavka_id,isNaqd) 
     values (?,1 ,now(),?,?,?,?,?,?,1) ;`,
                     [req.session.userId||null, fish, tel, viloyat, tuman, mfy, req.body.dostavka_id||1])
                 .then((rest) => {
-                    let {
-                        data
-                    } = JSON.parse(req.body.praduct_id), s = "", a = [], notFounds = [], lessProd = []
+                    let   data= req.body.product_id, s = "", a = [], notFounds = [], lessProd = []
                     data.forEach((e, i) => {
                         s += "SELECT *,cost cost2 FROM product WHERE id=? and isActive=1;";
                         a.push(e.product_id)
@@ -168,6 +165,7 @@ app.post("/naqd", async (req, res) => {
                                 })
                             } else {
                                 if (rows[i][0].count * 1 < e.count*1) {
+                                    console.log(rows[i], e.count*1)
                                     lessProd.push({
                                         id: e.product_id,
                                         name: e.name,
