b4a9ed879628f34e3ab1d1f3983bd0a90132e58e
mirazizergashev <mirazizmirmuxsinovich@gmail.com>
2021-10-30 09:01:52 +0500

    * qillim

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ var app = express();
 
 app.use((req, res, next) => {
 
-    res.header('Access-Control-Allow-Origin', '*');
-    // res.header('Access-Control-Allow-Origin', req.header('origin'));
+    // res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', req.header('origin'));
     res.setHeader("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,PATCH,OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.setHeader('Access-Control-Allow-Credentials', true)
