79c444aade8152507253e6184a5f3d3a53f65c21
Coder <oybekakramov0@gmail.com>
2021-11-24 22:40:14 -0500

    123

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var app = express();
 const whitelist = ["http://localhost:80","http://localhost:3003",
 "http://localhost:3004","http://localhost:3005",
 "http://buy-it.uz:80","http://a.buy-it.uz:3003",
-"http://a.buy-it.uz:3004","http://a.buy-it.uz:3005"
+"http://a.buy-it.uz:3004","http://a.buy-it.uz:3005","http://a.buy-it.uz:3004"
 ,
 "http://buy-it.uz","http://a.buy-it.uz","http://ams.buy-it.uz","http://cms.buy-it.uz"]
 const corsOptions = {
